d54d52116d5a35162aabcfe8efb340acf6b30877
Anders <anders@pop-os.localdomain>
2019-12-09 11:00:17 +0100

    Stuff now works.

--- elreverso.py
+++ elreverso.py
@@ -7,9 +7,6 @@ import matplotlib.pyplot as plt
 from scipy.optimize import nnls,lsq_linear
 from scipy.sparse.linalg import spsolve
 
-import ps_utils_test
-from ps_utils_test import *
-
 
 #From Fracois ps_utils
 def read_data_file(filename):
@@ -56,8 +53,10 @@ def PhotometricStereo(imgArr,mask,LightDirections):
     #The indices we actually want to calculate on
     notMasked = np.nonzero(mask)
 
-    m  = (np.linalg.inv(LightDirections) @ imgArr[notMasked].T)
-    
+    if len(LightDirections) == 3:
+        m  = (np.linalg.inv(LightDirections) @ imgArr[notMasked].T)
+    else:
+        m =  (np.linalg.pinv(LightDirections) @ imgArr[notMasked].T)
     ρ = np.linalg.norm(m,axis=0)
     
     normals = 1/ρ * m
@@ -73,29 +72,34 @@ def PhotometricStereo(imgArr,mask,LightDirections):
     
     return(n1,n2,n3)
 
-def NormalsToDivergence(Normals):
-
-    dx = -Normals[0]/Normals[2]
-    dy = -Normals[1]/Normals[2]
-    return dx + dy
 
 
+#FIXME: Multiply by ½ (*)?
+def CentralDifference(p,q):
+    """
+    p is horizontal, q is vertiacal
+    see page 7 for deffintion
+    """
 
-def Amaker2(p,q,mask = None):
-    N = p.shape[0]
-    M = p.shape[1]
-    λ = 1.
+    n,m = p.shape
 
-    Omega_padded = np.pad(mask, (1,1), mode='constant', constant_values=0)
-    down = mask - Omega_padded[2:,1:-1]*mask
-    up = mask - Omega_padded[:-2,1:-1]*mask
-    right = mask - Omega_padded[1:-1,2:]*mask
-    left = mask - Omega_padded[1:-1,:-2]*mask
+    left = np.append([0],np.arange(0,n-1))
+    right = np.append(np.arange(1,n),[n-1])
 
-    boundary = down + up + left + right
+    horizontalDiff = p[right,:] - p[left,:]
+    verticalDiff = q[:,right] - q[:,left]
+
+    return (1/2)*(horizontalDiff + verticalDiff)
+
+
+def Amaker(mask):
+    """
+    Creates the linear equation system for solving a 2D poisson problem,
+    Assumes that the image is square?(*)
+    """
     
-    rmask = mask.ravel()
-    nmask  = (boundary + 4*mask) >0
+    N = mask.shape[0]
+    M = mask.shape[1]
     
     A = lil_matrix((M*N, M*N))
 
@@ -117,9 +121,6 @@ def Amaker2(p,q,mask = None):
         if i - N >= 0:
             A[i - N,i] = -1.  * mask[np.mod(i-N,N),int((i - N )/N)]
 
-        # if boundary[np.mod(i,N),int(i/N)] > 0:
-        #     A[i,i] = 1
-
 
     return A
 
@@ -127,7 +128,7 @@ def Amaker2(p,q,mask = None):
 #FIXME: der sker mærkelige ting foruden transponering! (*)
 def PoissonSolver2D(normals,mask):
     """
-    Solves in the 2D case, 0 on the boundary 
+    Solves the 2D case of the possion problem given unit normals
     """
 
     p = -normals[0]/normals[2]
@@ -136,46 +137,23 @@ def PoissonSolver2D(normals,mask):
     #Find ud af hvilken form X har
     N = p.shape[0]
     M = p.shape[1]
-    b = (cdx(p) + cdy(q)).T.ravel()#(p + q).T.ravel()
-    # b = ps_utils_test.cdx(p) + ps_utils_test.cdy(q) 
-    # b = np.ones_like(p).ravel()
-    # b = np.flip(p+ q,axis = 0).T.ravel()
-    #b = (p + q)[mask > 0]
+    b = CentralDifference(p,q).T.ravel()
 
-    #1 if X is square
+    A = Amaker(mask)
 
-    A = Amaker2(p,q,mask)
-    # A = csr_matrix(bmat(A))
-
-    #A = csr_matrix(Amaker(X,mask))
-
-    #return (A,b,lsq_linear(A,b,verbose = 2).x)#implementer egen (*)
-    # res = lsq_linear(A,b,verbose = 2).x
-    # z = res.reshape((N,M))
     z = spsolve(A,b)
     z = -z.reshape((N,M)).T
     return (A,b,z)
 
 
-testX = np.arange(0,25,1).reshape((5,5))
-
-stencil = np.array([[0,1,0],[1,1,1],[0,1,0]])
-
-ndimage.convolve(testX,stencil,mode = "constant")
 
 
-#testRes = PoissonSolver2D(testX,testX)
-#print(np.linalg.norm(testRes[0].dot(testRes[2]) - testRes[1]))
-
-DataPath = "../Specialprojekt/tilAnders/Beethoven.mat"
+DataPath = "../../Specialprojekt/tilAnders/Beethoven.mat"
 
 Images, mask, S = read_data_file(DataPath)
 
 
 normals = PhotometricStereo(Images,mask,S)
-
-div = NormalsToDivergence(normals)
-
 
 
 res = PoissonSolver2D(normals,mask)
@@ -183,11 +161,10 @@ res = PoissonSolver2D(normals,mask)
 reshaped = res[2]
 
 
-#display_surface(reshaped)
+p = -normals[0]/normals[2]
+q = -normals[1]/normals[2]
 
-#print(np.linalg.norm(res[0].dot(res[2])-res[1]))
-# Images[:,:,0][np.where(mask)]
-
+cd = CentralDifference(p,q)
 
 
 _,(ax1,ax2,ax3) = plt.subplots(1,3)
@@ -197,38 +174,13 @@ ax3.imshow(normals[2])
 plt.show()
 
 _,(ax1,ax2,ax3) = plt.subplots(1,3)
-ax1.imshow(-normals[0]/normals[2])
-ax2.imshow(-normals[1]/normals[2])
-ax3.imshow(NormalsToDivergence(normals))
+ax1.imshow(p)
+ax2.imshow(q)
+ax3.imshow(cd)
 plt.show()
 
 
 
 plt.imshow(reshaped)
 plt.show()
-ps_utils_test.display_surface(reshaped)
-
-
-# z = simchony_integrate(normals[0],normals[1],normals[2],mask)
-
-
-# plt.imshow(z)
-# plt.show()
-
-# plt.imshow(z[0] - res[2])
-# plt.show()
-
-
-
-
-"""
-testN = np.array([[1,0,0],[0,1,0],[0,0,1]])
-testimg = np.ones((5,3,10))
-testimg2 = np.array([[[2.0,3.0,4.0],[0.0,1.0,2.0]]])
-nmatrix = np.dot(testN,testimg)
-for i in range(0,5):
-    for j in range(0,10):
-        print(nmatrix[:,i,j] == np.dot(testN,testimg[i,:,j]))
-        
-PhotometricStereo(testimg2,testN)
-"""
+display_surface(reshaped)
